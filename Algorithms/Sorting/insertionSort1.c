695a28052c7b71386b0c7c626fdb46d9f53f149c
thomastan <thomas.tan@msn.com>
2016-04-24 07:14:58 -0700

    Added solution for "Insertion Sort - Part 1"

---
+++ Algorithms/Sorting/insertionSort1.c
@@ -0,0 +1,33 @@
+// http://hackerrank.com/challenges/insertionsort1
+#include <stdio.h>
+
+void insertionSort(int N, int arr[]) {
+	int i, j, value, k;
+
+	for (i = 1; i < N; i++) {
+		value = arr[i];
+		for (j = i - 1; j >= 0 && value < arr[j]; j--) {
+			arr[j + 1] = arr[j];
+
+			for (k = 0; k < N; k++)
+				printf("%d%c", arr[k], k == N - 1 ? '\n' : ' ');
+		}
+
+		a[j + 1] = value;
+	}
+
+	for (k = 0; k < N; k++)
+		printf("%d%c", arr[k], k == N - 1 ? '\n' : ' ');
+}
+
+int main() {
+	int N;
+	scanf("%d", &N);
+
+	int arr[N], i;
+	for (i = 0; i < N; i++)
+		scanf("%d", &arr[i]);
+
+	insertionSort(N, arr);
+	return 0;
+}
