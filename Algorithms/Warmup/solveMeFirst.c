cd098b663eaf06e1b5435e20298ff0e5b75d2924
thomastan <thomas.tan@msn.com>
2016-05-29 01:14:15 -0700

    Added solution for "Solve Me First"

---
+++ Algorithms/Warmup/solveMeFirst.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int solveMeFirst(int a, int b) {
+	return a + b;
+}
+int main() {
+	int num1, num2;
+	scanf("%d %d", &num1, &num2);
+	printf("%d", solveMeFirst(num1, num2));
+	return 0;
+}
